7fb36fe6b71be82d5397814a50ffd69c9c29fe11
Fraz Aslam <77319791+frazaslam99@users.noreply.github.com>
2021-03-18 16:29:55 +0500

    Corona Virus App

--- src/Components/Country/country.js
+++ src/Components/Country/country.js
@@ -1,12 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import InputLabel from "@material-ui/core/InputLabel";
-import MenuItem from "@material-ui/core/MenuItem";
-import FormHelperText from "@material-ui/core/FormHelperText";
 import FormControl from "@material-ui/core/FormControl";
-import Select from "@material-ui/core/Select";
 import { getALLCountries } from "../../api";
-import { NativeSelect, Typography } from "@material-ui/core";
+import { NativeSelect} from "@material-ui/core";
 
 function CountryPicker({ handleChange, country }) {
   useEffect(() => {
